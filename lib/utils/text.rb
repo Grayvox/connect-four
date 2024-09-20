fbb5b69a8b64071475630da9759d8fd07d457a1c
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-09-20 11:20:09 -0400

    refactor!: Change board viewing system (FINISHED)

--- lib/utils/text.rb
+++ lib/utils/text.rb
@@ -2,10 +2,13 @@
 
 # Module containing text outputs for the game
 module Text
-  def show_board(board)
-    %(
-      #{board}
-    )
+  def build_visual(board, result = '', y_coord = 0, x_coord = 0)
+    return result if y_coord > 5
+
+    return build_visual(board, result += "|\n", y_coord + 1, 0) if x_coord > 6
+
+    result += "| #{board[y_coord][x_coord]} "
+    build_visual(board, result, y_coord, x_coord + 1)
   end
 
   def name_prompt(num)
