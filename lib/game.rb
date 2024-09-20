aef12d284b5cdbfc61044276a5c01472f79f6e1f
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-09-20 10:37:37 -0400

    feat!: Add customizable player symbols

--- lib/game.rb
+++ lib/game.rb
@@ -16,9 +16,9 @@ class Game
 
   public
 
-  def initialize
+  def initialize(symbol_one, symbol_two)
     @board = Board.new
-    @possible_symbols = ['☀', '☁']
+    @possible_symbols = [symbol_one, symbol_two]
     @spots_taken = 0
     @player_one = add_player(1)
     @player_two = add_player(2)
